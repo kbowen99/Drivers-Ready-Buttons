e44b6dec5389a52fcec47ec5497b760757419341
kbowen99 <kurtisbowen99@gmail.com>
2019-01-27 13:09:42 -0600

    Eric Wants files

---
+++ Drivers Ready Buttons/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include <Adafruit_CPlay_NeoPixel.h
+#include <avr/power.h>
+
+#define PIN            4
+#define buttonPin      3
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
