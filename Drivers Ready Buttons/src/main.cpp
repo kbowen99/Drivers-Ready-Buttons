f8d314c440250b1d6719193033f434bc8eb9cd1c
kbowen99 <kurtisbowen99@gmail.com>
2019-01-27 13:36:47 -0600

    Demo Code its kinda shitty

--- Drivers Ready Buttons/src/main.cpp
+++ Drivers Ready Buttons/src/main.cpp
@@ -1,14 +1,60 @@
 #include <Arduino.h>
-#include <Adafruit_CPlay_NeoPixel.h
+#include <Adafruit_Neopixel.h>
 #include <avr/power.h>
 
-#define PIN            4
-#define buttonPin      3
+#define neopixelPin     4
+#define buttonPin       3
+#define NUMPIXELS      12
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, neopixelPin, NEO_GRB + NEO_KHZ800);
+
+int spiralize_delay = 50;
+int buttonInst = 2;
+
 
 void setup() {
-  // put your setup code here, to run once:
+  //XBee Communication
+  Serial.begin(115200);
+  //Initialize Neopixel Ring
+  pixels.begin();
+  //Setup Button Input
+  pinMode(buttonPin, INPUT_PULLUP);
+}
+
+void setSpiralColor(int r, int g, int b){
+  for(int i=0;i<NUMPIXELS;i++){
+      pixels.setPixelColor(i, pixels.Color(r,g,b));
+      pixels.show();
+      delay(spiralize_delay);
+    } 
+}
+
+void hardcodedColors(int s){
+  switch (s){
+    case 0:
+    setSpiralColor(255,255,255);
+    break;
+    case 1:
+    setSpiralColor(255,0,0);
+    break;
+    case 2:
+    setSpiralColor(0,255,0);
+    break;
+    case 3:
+    setSpiralColor(0,0,255);
+    break;
+    default:
+    setSpiralColor(0,0,0);
+  }
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //Shitty Test Code
+  if (Serial.available()) {
+    hardcodedColors(Serial.read());
+  } else if (!digitalRead(buttonPin)) {
+    Serial.write(buttonInst);
+    hardcodedColors(buttonInst);
+  }
 }
